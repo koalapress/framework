e6a1c8a4c59f5d9d4952ad2b006fe458184a85d3
Ben Kremer <git@benkremer.de>
2025-08-04 15:04:16 +0200

    fix: acf cache

--- src/KoalaPress/Model/Traits/HasACF.php
+++ src/KoalaPress/Model/Traits/HasACF.php
@@ -6,34 +6,43 @@ use KoalaPress\Model\Casts\ACF;
 
 trait HasACF
 {
-	/**
-	 * Initialize the HasACF trait
-	 */
-	public function initializeHasACF(): void
-	{
-		self::retrieved(function ($model) {
-			$acf_fields = collect(get_field_objects($this->getAcfKey(), false, false))->keys();
-			$native_fields = collect($this->getAttributes())->keys();
-			$acf_casts = $acf_fields->diff($native_fields)->mapWithKeys(function (string $item) {
-				return [$item => ACF::class ];
-			});
-			$this->mergeCasts($acf_casts->toArray());
-		});
-	}
+    protected static array $acfFieldCache = [];
 
-	/**
-	 * return the acf key following ACFs convention for the current post or term
-	 */
-	public function getAcfKey(): ?string
-	{
-		return is_a($this, Taxonomy::class) ? 'term_' . $this->getAttribute('term_id') : $this->getAttribute('ID');
-	}
+    /**
+     * Initialize the HasACF trait
+     */
+    public function initializeHasACF(): void
+    {
+        self::retrieved(function ($model) {
+            $key = $this->getAcfKey();
+            if (!isset(self::$acfFieldCache[$key])) {
+                self::$acfFieldCache[$key] = collect(get_fields($key) ?? [])->keys();
+            }
 
-	/**
-	 * return the acf fields for the current post or term
-	 */
-	public function getAcfAttribute()
-	{
-		return get_fields($this->ID);
-	}
+            $acf_fields = self::$acfFieldCache[$key];
+            $native_fields = collect($this->getAttributes())->keys();
+
+            $acf_casts = $acf_fields->diff($native_fields)->mapWithKeys(function ($item) {
+                return [$item => ACF::class];
+            });
+
+            $this->mergeCasts($acf_casts->toArray());
+        });
+    }
+
+    /**
+     * return the acf key following ACFs convention for the current post or term
+     */
+    public function getAcfKey(): ?string
+    {
+        return is_a($this, Taxonomy::class) ? 'term_' . $this->getAttribute('term_id') : $this->getAttribute('ID');
+    }
+
+    /**
+     * return the acf fields for the current post or term
+     */
+    public function getAcfAttribute()
+    {
+        return get_fields($this->ID);
+    }
 }
